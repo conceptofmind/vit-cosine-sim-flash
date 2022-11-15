86515ea4aacd20c6898e66d5e5d9e909e1ea3846
Enrico Shippole <enricoship@gmail.com>
2022-11-15 18:42:27 -0500

    all

--- train.py
+++ train.py
@@ -19,7 +19,7 @@ args = parser.parse_args()
 DEVICE = 'cuda'
 IMAGE_SIZE = 224
 BATCH_SIZE = args.batch_size
-LEARNING_RATE = 6e-4
+LEARNING_RATE = 2e-4
 EPOCHS = 100
 
 train_transform = T.Compose([
